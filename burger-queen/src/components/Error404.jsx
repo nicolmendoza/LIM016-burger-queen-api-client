8dc77f24e08fc1a51d12cb6eea980dcf7fbc236a
Merly <merly2257@gmail.com>
2022-02-14 23:52:33 -0500

    protegiendo rutas y ruta 404

---
+++ burger-queen/src/components/Error404.jsx
@@ -0,0 +1,14 @@
+import {Button} from '../style-components/components'
+
+const Error404 = () => {
+
+    return(
+        <div className="login-full-container">
+            <p>Error 404</p>
+            <p>Pagina no encontrada</p>
+            <Button>Regresar</Button>
+        </div>
+    )
+}
+
+export default Error404
